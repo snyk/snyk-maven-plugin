878580efba9263e87ab16f598ed34443d934d96f
Jahed Ahmed <jahed@snyk.io>
2021-03-09 12:48:48 +0000

    chore: organise imports

--- src/it/test-without-dependency/verify.groovy
+++ src/it/test-without-dependency/verify.groovy
@@ -1,11 +1,9 @@
-// for known issues, no vulnerable paths found.
-import java.io.*;
 import org.codehaus.plexus.util.*;
 
-String log = FileUtils.fileRead( new File( basedir, "build.log" ) );
+String log = FileUtils.fileRead(new File(basedir, "build.log"));
 
 if(!log.contains("for known issues, no vulnerable paths found.")) {
-    throw new Exception("Missing dependency: axis:axis@1.4");
+    throw new Exception("`snyk test` success output not found");
 }
 
 return true;
--- src/main/java/io/snyk/SnykTestMojo.java
+++ src/main/java/io/snyk/SnykTestMojo.java
@@ -1,18 +1,12 @@
 package io.snyk;
 
-
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugin.logging.Log;
 
 import java.util.List;
 
-/**
- * Goal which touches a timestamp file.
- */
 @Mojo(name = "test")
 public class SnykTestMojo extends AbstractMojo {
     @Parameter(property = "apiToken")
@@ -24,10 +18,9 @@ public class SnykTestMojo extends AbstractMojo {
     @Parameter(property = "args")
     private List<String> args;
 
-    public void execute() throws MojoFailureException {
+    public void execute() {
         final Log log = getLog();
         log.info("for known issues, no vulnerable paths found.");
-
         /*
             1. validate plugin options
                 - apiKey
