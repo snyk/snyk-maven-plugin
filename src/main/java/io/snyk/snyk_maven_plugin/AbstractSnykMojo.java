c624ab4ec7f0b2e3889d50f8934637707b9efbfe
maxjeffos <44034094+maxjeffos@users.noreply.github.com>
2021-03-11 12:44:01 -0500

    chore: run build on variety of operating systems

--- src/main/java/io/snyk/snyk_maven_plugin/AbstractSnykMojo.java
+++ src/main/java/io/snyk/snyk_maven_plugin/AbstractSnykMojo.java
@@ -1,25 +1,22 @@
 package io.snyk.snyk_maven_plugin;
 
-import com.google.common.annotations.VisibleForTesting;
 import io.snyk.snyk_maven_plugin.command.Command;
 import io.snyk.snyk_maven_plugin.command.CommandLine;
 import io.snyk.snyk_maven_plugin.command.CommandRunner;
-import io.snyk.snyk_maven_plugin.download.GitHubDownloader;
+import io.snyk.snyk_maven_plugin.download.ExecutableDownloader;
 import io.snyk.snyk_maven_plugin.download.Installer;
 import io.snyk.snyk_maven_plugin.download.Platform;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public abstract class AbstractSnykMojo extends AbstractMojo {
@@ -52,7 +49,7 @@ public abstract class AbstractSnykMojo extends AbstractMojo {
                     try {
                         Platform platform = Platform.current();
                         Path targetFolder = Installer.getInstallLocation(platform, Optional.ofNullable(System.getProperty("user.home")).map(Paths::get), System.getenv());
-                        return GitHubDownloader.download(targetFolder, Platform.current());
+                        return ExecutableDownloader.download(targetFolder, Platform.current());
                     } catch (IOException e) {
                         throw new RuntimeException("failed to download executable", e);
                     }
---
+++ src/main/java/io/snyk/snyk_maven_plugin/download/ExecutableDownloader.java
@@ -0,0 +1,29 @@
+package io.snyk.snyk_maven_plugin.download;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Path;
+
+import static java.lang.String.format;
+
+public class ExecutableDownloader {
+    private static final String SNYK_RELEASES_LATEST = "https://static.snyk.io/cli/latest/%s";
+
+    public static File download(Path destination, Platform platform) throws IOException {
+        URL url = new URL(format(SNYK_RELEASES_LATEST, platform.snykExecutableFileName));
+        destination.toFile().mkdirs();
+        File file = destination.resolve(platform.snykExecutableFileName).toFile();
+        try (
+            ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+            FileOutputStream fos = new FileOutputStream(file);
+        ) {
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        }
+        file.setExecutable(true);
+        return file;
+    }
+}
