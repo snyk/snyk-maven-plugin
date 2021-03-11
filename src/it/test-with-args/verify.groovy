b743163c038a09efaae4b80f9ba8b80143af0aa5
Jahed Ahmed <jahed@snyk.io>
2021-03-11 11:50:29 +0000

    test: split args to its own test scenario

---
+++ src/it/test-with-args/verify.groovy
@@ -0,0 +1,9 @@
+import org.codehaus.plexus.util.FileUtils;
+
+String log = FileUtils.fileRead(new File(basedir, "build.log"));
+
+if (!log.contains("io.snyk.it:test-without-dependency @ 1.0-SNAPSHOT")) {
+    throw new Exception("`snyk test --print-deps` --print-deps output not found");
+}
+
+return true;
